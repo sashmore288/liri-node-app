645c773d975da17d3af138268c8e48268febb5e5
StaShaun <31264703+sashmore288@users.noreply.github.com>
2017-12-19 09:04:14 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "FkV0HGXOysnnk5slgorgWeXDh",
+  consumer_secret: "OzP9RlnHicu9Y8wh8PHkaE57bnEW2jyuE0jFVJqxtUIyOkSBzW",
+  access_token_key: "941025462523973632-1DbS805ZR2fAwyx37LWLt94JF8RRQ4y",
+  access_token_secret: "RWwd2ROqUhDhKJ91FxiiW7cFw1cULtHFyOWcAWjwDLKbi",
+};
+
+module.exports = twitterKeys;
